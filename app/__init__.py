7100bd85ac61958c6325ae58ca8b51867001d69f
swaroopms26 <167847823+swaroopms26@users.noreply.github.com>
2025-12-14 10:02:03 -0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,16 @@
+from dotenv import load_dotenv
+load_dotenv()
 
+from app import create_app, db
+from app.models import User, LoanApplication
+
+app = create_app()
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'LoanApplication': LoanApplication}
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, use_reloader=False, port=5001)
